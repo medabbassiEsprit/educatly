f665b6fcae8f45cc837a3a6abe9e05af8c59819a
khmayssa <mayssa.khalifa@esprit.tn>
2022-07-03 18:25:37 +0100

    Dashborad mayssa

--- src/GUI/FXML_DashboardStudentController.java
+++ src/GUI/FXML_DashboardStudentController.java
@@ -5,15 +5,10 @@
  */
 package GUI;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -25,14 +20,9 @@ import javafx.scene.layout.VBox;
  */
 public class FXML_DashboardStudentController implements Initializable {
 
-    @FXML
     private VBox pnl_scroll;
     @FXML
-    private Label lbl_currentprojects;
-    @FXML
-    private Label lbl_pending;
-    @FXML
-    private Label lbl_completed;
+    private Label btn_exit;
 
     /**
      * Initializes the controller class.
@@ -40,29 +30,12 @@ public class FXML_DashboardStudentController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-         refreshNodes();
+         
     }    
 
     @FXML
     private void handleButtonAction(MouseEvent event) {
-         refreshNodes();
+        System.exit(0);
     }
-     private void refreshNodes()
-    {
-        pnl_scroll.getChildren().clear();
-        
-        Node [] nodes = new  Node[15];
-        
-        for(int i = 0; i<10; i++)
-        {
-            try {
-                nodes[i] = (Node)FXMLLoader.load(getClass().getResource("Item.fxml"));
-               pnl_scroll.getChildren().add(nodes[i]);
-                
-            } catch (IOException ex) {
-                Logger.getLogger(FXML_DashboardStudentController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-           
-        }  
-    }
+     
 }
